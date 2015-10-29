2cb7a3c224264df559f4dd2cda57a53fa44e64f2
孙晨 <sunchen@jrq.com>
2015-10-29 19:17:08 +0800

    + class decorator

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 # notify function
 def notify(title='', message='', url='', group='', execute=''):
@@ -21,3 +22,34 @@ def logger(name=__name__, msg=''):
     f = open(path, 'a')
     f.write(msg + '\n')
     f.close()
+
+# testA => test_a
+def camelToUnderline(string):
+    result = ''
+    for char in string:
+        result += char if char.islower() else '_' + char.lower()
+    return result
+
+# test_a => testA
+def underlineToCamel(string):
+    result = ''
+    for substr in string.split('_'):
+        result += substr if result == '' else substr.capitalize()
+    return result
+
+# in-class decorator
+# it calculate total time cost by the function().
+# need to specify a class attribute(accum)
+def timerAccumulate(accum):
+    def timer(f):
+        def _func(*args, **kw):
+            s = time.time()
+            r = f(*args, **kw)
+            # arg[0] is self (instance)
+            t = getattr(args[0], accum)+ time.time() - s
+            setattr(args[0], accum, t)
+            return r
+        return _func
+    return timer
+
+
