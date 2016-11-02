4793e12253d53684c53e7d1982bfc62473cab784
孙晨 <sunchen@jrq.com>
2016-11-02 15:09:48 +0800

    * update notify

--- functions.py
+++ functions.py
@@ -7,13 +7,14 @@ import time
 
 
 def notify(title='', message='', url='', group='', execute=''):
-    t = '-title {!r}'.format(title)
-    m = '-message {!r}'.format(message)
-    u = '-open {!r}'.format(url)
-    g = '-group {!r}'.format(group)
-    e = '-execute {!r}'.format(execute)
+    t = '-title "%s"' % (title)
+    m = '-message "%s"' % (message)
+    u = '-open "%s"' % (url)
+    g = '-group "%s"' % (group)
+    e = '-execute "%s"' % (execute)
+    print ' '.join([m, t, u, g, e])
     os.system(
-        '/usr/local/bin/terminal-notifier {}'.format(' '.join([m, t, u, g, e])))
+        '/usr/local/bin/terminal-notifier %s' % (' '.join([m, t, u, g, e])))
 
 # if unicode char is chinese
 
